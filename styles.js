a3ae9f6e0e373ff5219665737a38046fa3a1c749
JuanPiece2025 <vivenesjuan332@gmail.com>
2025-04-11 14:48:48 -0400

    Update styles.js

--- styles.js
+++ styles.js
@@ -3,7 +3,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/TU_ID_DE_WIDGET/default';
+s1.src='https://embed.tawk.to/66c711c250c10f7a009f31e3/1i5spqopk';
 s1.charset='UTF-8';
 s0.parentNode.insertBefore(s1,s0);
 })();
